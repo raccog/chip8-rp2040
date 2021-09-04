f122dedb64ec195185dc2c564ff73b85c66cd372
Ryan Cohen <rcohenprogramming@gmail.com>
2021-09-03 23:59:53 -0400

    Updated the rom server to send any amount of roms to be saved in the rp2040 flash (memory is not boundary checked yet).
    The emulator will be put into receiver mode if the button on GPIO 20 is held down on power up.
    Once in receiver mode, the rom server can send roms over UART to be saved to flash.
    
    Currently, the first program on the index is run on initialization, but programs will be selectable in the future.

--- chip8.c
+++ chip8.c
@@ -13,7 +13,7 @@
 #define DISPLAY_HEIGHT 32
 #define DISPLAY_SIZE DISPLAY_WIDTH * DISPLAY_HEIGHT
 
-#define ONE_HERTZ 1000000
+#define ONE_HERTZ 1000000 / 60
 #define CLOCK_CYCLE_LENGTH 2000
 
 #define ROM_START 0x200
@@ -24,6 +24,7 @@
 #define FONT_START 0x50
 #define FONT_SIZE 0x50
 #define FONT_CHAR_SIZE 0x5
+#define ROM_BUFFER_SIZE 0x100
 
 #define N_MASK 0b10000000
 #define NN_MASK 0b01000000
@@ -34,6 +35,7 @@
 
 #define VX processor->V[get_X(instruction)]
 #define VY processor->V[get_Y(instruction)]
+#define VF processor->V[0xf]
 
 typedef uint8_t byte;
 typedef uint16_t word;
@@ -91,38 +93,50 @@ typedef struct {
     byte SP;
     byte delay;
     byte sound;
-    bool key_buffer[KEY_COUNT];
-    uint64_t key_last_irq[KEY_COUNT];
+    volatile bool key_buffer[KEY_COUNT];
     bool keys[KEY_COUNT];
     bool should_draw;
     uint64_t last_cycle_time;
     uint64_t last_timer_decrement_time;
-} Processor;
+} processor_t;
+
+typedef uint16_t rom_count_t;
 
 typedef struct {
     uint16_t size;
-    uint8_t title_size;
-    char *title;
-    uint8_t *rom;
-} Rom;
+    uint32_t flash_offset;
+} rom_meta_t;
 
-typedef struct {
-    uint16_t rom_count;
-    Rom *roms;
-} RomList;
+rom_count_t uart_write_roms_to_flash() {
+    // get number of bytes that will be written to flash
+    uint8_t buffer[FLASH_PAGE_SIZE];
+    uart_read_blocking(uart0, buffer, sizeof(uint32_t));
 
-RomList read_metadata_uart() {
-    RomList metadata;
-    metadata.roms = malloc(sizeof(Rom));
-    uart_read_blocking(uart0, (uint8_t *)&metadata.rom_count, 2);
-    uart_read_blocking(uart0, (uint8_t *)&metadata.roms->size, 2);
-    uart_read_blocking(uart0, &metadata.roms->title_size, 1);
-    metadata.roms->title = malloc(metadata.roms->title_size + 1);
-    metadata.roms->rom = malloc(metadata.roms->size);
-    uart_read_blocking(uart0, metadata.roms->title, metadata.roms->title_size);
-    *(metadata.roms->title + metadata.roms->title_size) = '\0';
-    uart_read_blocking(uart0, metadata.roms->rom, metadata.roms->size);
-    return metadata;
+    const uint32_t BYTES_WRITTEN = *(uint32_t *)buffer;
+    const uint16_t FULL_PAGES_WRITTEN = BYTES_WRITTEN / FLASH_PAGE_SIZE;
+    const uint8_t FLASH_SECTOR_PAGES = FLASH_SECTOR_SIZE / FLASH_PAGE_SIZE;
+
+    // write all full pages to flash
+    for (uint16_t i = 0; i < FULL_PAGES_WRITTEN; ++i) {
+        if (i % FLASH_SECTOR_PAGES == 0) {
+            flash_range_erase(FLASH_TARGET_OFFSET + i * FLASH_PAGE_SIZE, FLASH_SECTOR_SIZE);
+        }
+        uart_read_blocking(uart0, buffer, FLASH_PAGE_SIZE);
+        flash_range_program(FLASH_TARGET_OFFSET + i * FLASH_PAGE_SIZE, buffer, FLASH_PAGE_SIZE);
+    }
+
+    // write final partial page if needed
+    const uint8_t EXTRA_BYTES_WRITTEN = BYTES_WRITTEN % FLASH_PAGE_SIZE;
+    if (EXTRA_BYTES_WRITTEN != 0) {
+        if (FULL_PAGES_WRITTEN % FLASH_SECTOR_PAGES == 0) {
+            flash_range_erase(FLASH_TARGET_OFFSET + FULL_PAGES_WRITTEN * FLASH_PAGE_SIZE, FLASH_SECTOR_SIZE);
+        }
+        memset(buffer, 0, FLASH_PAGE_SIZE); // clear buffer of any extra data
+        uart_read_blocking(uart0, buffer, EXTRA_BYTES_WRITTEN);
+        flash_range_program(FLASH_TARGET_OFFSET + FULL_PAGES_WRITTEN * FLASH_PAGE_SIZE, buffer, FLASH_PAGE_SIZE);
+    }
+
+    return *(rom_count_t *)flash_target_contents;
 }
 
 word get_opcode(word instruction) {
@@ -218,7 +232,7 @@ byte get_Y(word instruction) {
     return (instruction >> 4) & 0xf;
 }
 
-void processor_load_font(Processor *processor) {
+void processor_load_font(processor_t *processor) {
     const byte FONT[] = {
         0xf0, 0x90, 0x90, 0x90, 0xf0, // 0
         0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -240,12 +254,12 @@ void processor_load_font(Processor *processor) {
     memcpy(processor->memory + FONT_START, FONT, FONT_SIZE);
 }
 
-void processor_clear_displays(Processor *processor) {
+void processor_clear_displays(processor_t *processor) {
     memset(processor->display, 0, DISPLAY_SIZE);
     memset(processor->full_display, 0, DISPLAY_SIZE * 4);
 }
 
-void processor_reset(Processor *processor) {
+void processor_reset(processor_t *processor) {
     memset(processor->memory, 0, MEMORY_SIZE);
     memset(processor->V, 0, VARIABLE_COUNT);
     memset(processor->stack, 0, STACK_SIZE);
@@ -260,20 +274,23 @@ void processor_reset(Processor *processor) {
     processor_load_font(processor);
 }
 
-Processor processor_init() {
-    Processor processor;
+processor_t processor_init() {
+    processor_t processor;
     processor_reset(&processor);
     return processor;
 }
 
-word processor_next_instruction(Processor *processor) {
+word processor_next_instruction(processor_t *processor) {
     word instruction = ((word)processor->memory[processor->PC] << 8) | processor->memory[processor->PC + 1];
     processor->PC += 2;
     return instruction;
 }
 
-void processor_update_pixel(Processor *processor, word instruction, byte x, byte y, byte pixel) {
-    processor->display[VX + x + VY / 8 * DISPLAY_WIDTH] ^= pixel << y;
+bool processor_update_pixel(processor_t *processor, word instruction, byte x, byte y, byte pixel) {
+    size_t idx = VX + x + VY / 8 * DISPLAY_WIDTH;
+    bool collision = ((processor->display[idx] >> y) & 0x1 == pixel);
+    processor->display[idx] &= ~(0x1 << y);
+    processor->display[idx] |= (uint8_t)collision << y;
 
     for (byte i = 0; i < 2; ++i) {
         const size_t idx = (VX + x) * 2 + i + (VY + y) / 4 * DISPLAY_WIDTH * 2;
@@ -281,12 +298,42 @@ void processor_update_pixel(Processor *processor, word instruction, byte x, byte
         processor->full_display[idx] &= ~(0x3 << byte_shift);
         processor->full_display[idx] |= ((pixel << 1) | pixel) << byte_shift;
     }
+
+    return collision;
 }
 
-void processor_run_instruction(Processor *processor, word instruction) {
+bool processor_get_key_pressed(processor_t *processor, uint8_t *key) {
+    for (uint8_t i = 0; i < KEY_COUNT; ++i) {
+        if (processor->keys[i]) {
+            *key = i;
+            return true;
+        }
+    }
+    return false;
+}
+
+void processor_update_keys(processor_t *processor) {
+    for (uint i = 0; i < KEY_COUNT; ++i) gpio_set_irq_enabled(GPIO_KEYS[i], GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, false);
+    for (uint i = 0; i < KEY_COUNT; ++i) {
+        uint gpio = GPIO_KEYS[i];
+        uint key = GPIO_KEY_MAP[gpio];
+        uint gpio_state = gpio_get(gpio);
+        if (gpio_state != processor->key_buffer[key]) {
+            processor->key_buffer[key] = gpio_state;
+        }
+        processor->keys[key] = processor->key_buffer[key];
+    }
+    for (uint i = 0; i < KEY_COUNT; ++i) gpio_set_irq_enabled(GPIO_KEYS[i], GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+}
+
+SSD1306 ssd;
+
+void processor_run_instruction(processor_t *processor, word instruction) {
     switch (get_opcode(instruction)) {
         case CLEAR_SCREEN:
             processor_clear_displays(processor);
+            ssd1306_buffer_fill_pixels(&ssd, BLACK);
+            ssd1306_set_pixels(&ssd);
             break;
         case RETURN:
             --processor->SP;
@@ -328,18 +375,23 @@ void processor_run_instruction(Processor *processor, word instruction) {
             VX ^= VY;
             break;
         case VX_ADD_VY:
+            VF = (uint8_t)(256 - VY <= VX);
             VX += VY;
             break;
         case VX_SUB_VY:
+            VF = (uint8_t)(VX >= VY);
             VX -= VY;
             break;
         case VX_SHIFT_RIGHT:
+            VF = VX & 0x1;
             VX >>= VY;
             break;
         case VY_SUB_VX:
+            VF = (uint8_t)(VY >= VX);
             VY -= VX;
             break;
         case VX_SHIFT_LEFT:
+            VF = VX >> 7;
             VX <<= VY;
             break;
         case SKIP_NEQ_VY:
@@ -355,10 +407,13 @@ void processor_run_instruction(Processor *processor, word instruction) {
             VX = get_NN(instruction) & (rand() % 0xff);
             break;
         case DRAW:
+            VF = 0;
             for (byte y = 0; y < get_N(instruction); ++y) {
                 for (byte x = 0; x < 8; ++x) {
                     const byte pixel = (processor->memory[processor->I + y] >> (7 - x)) & 0x1;
-                    processor_update_pixel(processor, instruction, x, y, pixel);
+                    if (processor_update_pixel(processor, instruction, x, y, pixel)) {
+                        VF = 1;
+                    }
                 }
             }
             processor->should_draw = true;
@@ -373,6 +428,14 @@ void processor_run_instruction(Processor *processor, word instruction) {
             VX = processor->delay;
             break;
         case WAIT_KEY_PRESS:
+            {
+                uint8_t key;
+                while (!processor_get_key_pressed(processor, &key)) {
+                    processor_update_keys(processor);
+                    sleep_us(CLOCK_CYCLE_LENGTH);
+                }
+                VX = key;
+            }
             break;
         case DELAY_SET_VX:
             processor->delay = VX;
@@ -403,7 +466,7 @@ void processor_run_instruction(Processor *processor, word instruction) {
     }
 }
 
-void processor_decrement_timers(Processor *processor) {
+void processor_decrement_timers(processor_t *processor) {
     uint64_t current_time = to_us_since_boot(get_absolute_time());
     if (current_time - processor->last_timer_decrement_time > ONE_HERTZ) {
         processor->last_timer_decrement_time = current_time;
@@ -416,32 +479,22 @@ void processor_decrement_timers(Processor *processor) {
     }
 }
 
-void processor_update_keys(Processor *processor) {
-    for (uint i = 0; i < KEY_COUNT; ++i) gpio_set_irq_enabled(GPIO_KEYS[i], GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, false);
-    for (uint i = 0; i < KEY_COUNT; ++i) {
-        uint gpio = GPIO_KEYS[i];
-        uint key = GPIO_KEY_MAP[gpio];
-        uint gpio_state = gpio_get(gpio);
-        if (gpio_state != processor->key_buffer[key]) {
-            processor->key_buffer[key] = gpio_state;
-        }
-    }
-    memcpy(processor->keys, processor->key_buffer, KEY_COUNT);
-    for (uint i = 0; i < KEY_COUNT; ++i) gpio_set_irq_enabled(GPIO_KEYS[i], GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
-}
-
-void processor_run_cycle(Processor *processor) {
+int processor_run_cycle(processor_t *processor) {
     processor->last_cycle_time = to_us_since_boot(get_absolute_time());
     processor_update_keys(processor);
     const word instruction = processor_next_instruction(processor);
+#ifdef CHIP8_DEBUG
+    if (instruction == 0) return 1;
+#endif
     processor_run_instruction(processor, instruction);
     processor_decrement_timers(processor);
 #ifdef CHIP8_DEBUG
     printf("Instruction ran at 0x%x: 0x%x\n", processor->PC - 2, instruction);
 #endif
+    return 0;
 }
 
-Processor processor;
+processor_t processor;
 
 void gpio_irq_callback(uint gpio, uint32_t events) {
     gpio_set_irq_enabled(gpio, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, false);
@@ -474,33 +527,29 @@ int main() {
         gpio_pull_down(gpio);
         gpio_set_irq_enabled_with_callback(gpio, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_irq_callback);
     }
+    gpio_init(20);
+    gpio_set_dir(20, GPIO_IN);
+    gpio_pull_down(20);
 
-    SSD1306 ssd;
     ssd1306_init(&ssd, 0x3c, i2c1, BLACK);
     ssd1306_set_full_rotation(&ssd, true);
     printf("Screen initialized\n");
 
     processor = processor_init();
-    printf("Processor initialized\n");
+    printf("processor_t initialized\n");
 
-    while (true) {
-        processor_update_keys(&processor);
-        sleep_us(CLOCK_CYCLE_LENGTH);
+    if (gpio_get(20)) {
+        rom_count_t rom_count = uart_write_roms_to_flash();
     }
-
-/*
-    RomList metadata = read_metadata_uart();
-    printf("Rom count: %x\n", metadata.rom_count);
-    memcpy(processor.memory + ROM_START, metadata.roms->rom, metadata.roms->size);
-    uint8_t *rom = malloc(256);
-    memcpy(rom, metadata.roms->rom, metadata.roms->size);
-    flash_range_erase(FLASH_TARGET_OFFSET, FLASH_SECTOR_SIZE);
-    flash_range_program(FLASH_TARGET_OFFSET, rom, 256);
-*/
-    memcpy(processor.memory + ROM_START, flash_target_contents, 132);
+    rom_meta_t rom_meta;
+    rom_meta.size = *(uint16_t *)(flash_target_contents + 2);
+    rom_meta.flash_offset = *(uint32_t *)(flash_target_contents + 4);
+    char title[256];
+    strcpy(title, flash_target_contents + rom_meta.flash_offset);
+    memcpy(processor.memory + ROM_START, flash_target_contents + rom_meta.flash_offset + strlen(title) + 1, rom_meta.size);
 
     while (true) {
-        processor_run_cycle(&processor);
+        if (processor_run_cycle(&processor) == 1) exit(1);
         if (processor.should_draw) {
             processor.should_draw = false;
             ssd1306_buffer_set_pixels_direct(&ssd, processor.full_display);
